d9357eb2f2a63b0be93abed3f5466a8f780fcfef
Meunier Emmanuel <Emmanuel.meunier.dev@gmail.com>
2020-02-12 13:10:31 +0100

    section movie

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,22 +30,117 @@ document.querySelector("#buttonContact").addEventListener("click", () => {
 
 
 // API
+//genre 
 
-async function GetDataGenre() {
-    let data = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=fce8cbf1171b45ecfb9db01f98832cef&language=fr_FR")
-    let response = await data.json()
-    let arrayGenre = Array.from(response)
-    return arrayGenre
-}
+const genres = [{
+        "id": 28,
+        "name": "Action"
+    },
+    {
+        "id": 12,
+        "name": "Aventure"
+    },
+    {
+        "id": 16,
+        "name": "Animation"
+    },
+    {
+        "id": 35,
+        "name": "Comédie"
+    },
+    {
+        "id": 80,
+        "name": "Crime"
+    },
+    {
+        "id": 99,
+        "name": "Documentaire"
+    },
+    {
+        "id": 18,
+        "name": "Drame"
+    },
+    {
+        "id": 10751,
+        "name": "Familial"
+    },
+    {
+        "id": 14,
+        "name": "Fantastique"
+    },
+    {
+        "id": 36,
+        "name": "Histoire"
+    },
+    {
+        "id": 27,
+        "name": "Horreur"
+    },
+    {
+        "id": 10402,
+        "name": "Musique"
+    },
+    {
+        "id": 9648,
+        "name": "Mystère"
+    },
+    {
+        "id": 10749,
+        "name": "Romance"
+    },
+    {
+        "id": 878,
+        "name": "Science-Fiction"
+    },
+    {
+        "id": 10770,
+        "name": "Téléfilm"
+    },
+    {
+        "id": 53,
+        "name": "Thriller"
+    },
+    {
+        "id": 10752,
+        "name": "Guerre"
+    },
+    {
+        "id": 37,
+        "name": "Western"
+    }
+]
 
-async function GetDataTopRate() {
-    let data = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=fce8cbf1171b45ecfb9db01f98832cef&language=fr-FR&page=1")
+
+
+async function getupComing() {
+    let data = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=fce8cbf1171b45ecfb9db01f98832cef&language=fr-FR&page=1")
     let response = await data.json()
-    for (let i = 1; i < 5; i++) {
-        document.querySelector(`#film${i}`).setAttribute("src","https://image.tmdb.org/t/p/w200" + response.results[i].backdrop_path)
+    for (let i = 1; i <= 4; i++) {
+        document.querySelector(`#film${i}`).setAttribute("src", "https://image.tmdb.org/t/p/w500" + response.results[i].backdrop_path)
+        console.log(i);
+
     }
 }
-GetDataTopRate()
+getupComing()
+
+//movies 
+async function getDataTopRate() {
+    let data = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=fce8cbf1171b45ecfb9db01f98832cef&language=fr-FR&page=1")
+    let response = await data.json()
+    for (let i = 1; i <= 5; i++) {
+        document.querySelector(`#imgCard${i}`).setAttribute("src", "https://image.tmdb.org/t/p/w200" + response.results[i].backdrop_path)
+        document.querySelector(`#cardTitle${i}`).textContent = response.results[i].title
+        document.querySelector(`#cardDate${i}`).textContent = response.results[i].release_date
+        document.querySelector(`#cardGenre${i}`).textContent = response.results[i].
+        console.log(response)
+    }
+}
+getDataTopRate()
+
+
+
+
+//footer bouton
 let boutonVersTop = document.createElement("button");
 boutonVersTop.setAttribute("class", "col-1 col-md-1 offset-md-10");
 boutonVersTop.setAttribute("id", "arrowUp");
@@ -56,8 +151,10 @@ icon.setAttribute("class", "far fa-arrow-alt-circle-up fa-2x");
 icon.setAttribute("id", "imgLien")
 document.getElementById("arrowUp").appendChild(icon);
 
-document.getElementById("reseauxSociaux").addEventListener("click", () =>{
-    document.getElementById("header").scrollIntoView(true, {behaviour:"smooth"});
+document.getElementById("reseauxSociaux").addEventListener("click", () => {
+    document.getElementById("header").scrollIntoView(true, {
+        behaviour: "smooth"
+    });
 });
 
 let hauteurY = document.getElementById("header").offsetHeight;
